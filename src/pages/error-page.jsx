25d4015d06272f059565b9057b4b34a70747d413
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-14 23:39:26 +0300

    Error page and sales component

---
+++ src/pages/error-page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ErrorPage() {
+    return (
+        <div>
+            <h1>OOOOPS!</h1>
+            <p>something happened</p>
+        </div>
+    )
+}
