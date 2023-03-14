034b357a5317f61cfe4fb083b06684a300e63a33
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-14 15:03:13 +0300

    login assets

---
+++ src/components/logintest.jsx
@@ -0,0 +1,27 @@
+export const myArray = [
+    {
+        username: 'user1',
+        email: 'user1@example.com',
+        password: 'abcde12345'
+    },
+    {
+        username: 'user2',
+        email: 'user2@example.com',
+        password: 'fghij67890'
+    },
+    {
+        username: 'user3',
+        email: 'user3@example.com',
+        password: 'klmno12345'
+    },
+    {
+        username: 'user4',
+        email: 'user4@example.com',
+        password: 'pqrst67890'
+    },
+    {
+        username: 'user5',
+        email: 'user5@example.com',
+        password: 'uvwxy12345'
+    }
+];
