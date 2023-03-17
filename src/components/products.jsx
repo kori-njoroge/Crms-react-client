fd012cee4166109884bae5d17e899d8900c40888
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-17 10:09:08 +0300

    products page, imprement add item ,displaying all items, deleting an a item

---
+++ src/components/products.jsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from 'react';
+
+
+import '../styles/products.css'
+import AddProduct from './add-product-modal';
+import { products } from './testData/products-test'
+
+export default function Products() {
+    const [addProd, setAddProd] = useState(false);
+    const [prods, setProducts] = useState(products);
+    const [search, setSearch] = useState('');
+    const [filter, setFilter] = useState('');
+    const [showPopUp, setShowPopUp] = useState(prods?.map(() => false));
+
+    useEffect(() => {
+        function setFilterfunc() {
+            if (filter.trim()) {
+                setProducts(products?.filter(product => product.status === filter))
+            } else {
+                setProducts(products)
+            }
+        }
+        setFilterfunc();
+        return () => {
+            setProducts(products)
+        }
+    }, [filter])
+
+
+    function handleEllipsisClick(index) {
+        const popUp = showPopUp.map((_, i) => i === index)
+        setShowPopUp(popUp)
+    }
+
+    function handleEllipsisClose(index) {
+        const newShowPopUp = [...showPopUp];
+        newShowPopUp[index] = false;
+        setShowPopUp(newShowPopUp);
+    }
+    // function delete item
+    function deleteItem(id) {
+        setProducts(prods?.filter(item => item.id !== id))
+    }
+    // approve product
+    function approveProduct(id) {
+        prods.map(item => {
+            if (item.id === id) {
+                return item.status = 'In Market'
+            } else {
+                return item
+            }
+        })
+        console.log(prods)
+    }
+
+
+    useEffect(() => {
+        function handleEscapeKeyPress(event) {
+            if (event.key === 'Escape') {
+                const newShowPopUp = prods?.map(() => false);
+                setShowPopUp(newShowPopUp);
+            }
+        }
+        document.addEventListener('keydown', handleEscapeKeyPress);
+        return () => {
+            document.removeEventListener('keydown', handleEscapeKeyPress);
+        };
+    }, [prods]);
+
+    return (
+        <div className='modal-container'>
+            <div className="filters">
+                <h5 className={filter === "" ? "h5active" : ""} onClick={() => { setFilter(``) }} >All Products</h5>
+                <h5 className={filter === "Pending Approval" ? "h5active" : ""} onClick={() => { setFilter(`Pending Approval`) }} >Pending Approval</h5>
+                <h5 className={filter === "In Market" ? "h5active" : ""} onClick={() => { setFilter(`In Market`) }} >In Market</h5>
+                <h5 className={filter === "Sold Out" ? "h5active" : ""} onClick={() => { setFilter(`Sold Out`) }} >Sold Out</h5>
+            </div>
+            <div className="searchPanel">
+                <div className="search-box">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <input
+                        type="text"
+                        name="search"
+                        placeholder='Search by category'
+                        onChange={(event) => {
+                            setSearch(event.target.value)
+                        }}
+                    />
+                </div>
+                <button
+                    className='add-user-btn'
+                    onClick={() => { setAddProd(true) }}
+                >Add Product</button>
+            </div>
+            <table className='products-table' >
+                <thead>
+                    <tr>
+                        <td>#</td>
+                        <td>Name</td>
+                        <td>Description</td>
+                        <td>category</td>
+                        {/* <td>Date Added</td> */}
+                        <td>Status</td>
+                        <td>Remaining <br /> Pieces</td>
+                        <td>Price</td>
+                        <td>Actions</td>
+                    </tr>
+                </thead>
+                <tbody >
+                    {prods && prods.filter((item) => {
+                        return search.toLowerCase() === "" ? item : item.category.toLowerCase().includes(search)
+                    }).map((item, index) => (
+                        <tr className='actions-row' key={item.id}>
+                            <td className='sold-ou'>{item.id}</td>
+                            <td className='user--name'>{item.name}</td>
+                            <td className='table-desc'>{item.description}</td>
+                            <td>{item.category}</td>
+                            <td>{item.status}</td>
+                            <td className={item.remainingPieces === 0 ? "sold-out actions" : 'actions'}>{item.remainingPieces}</td>
+                            <td className='actions'>{item.price}</td>
+                            <td className='actions' onClick={() => { handleEllipsisClick(index) }}><i className="fa-solid fa-ellipsis-vertical" ></i></td>
+                            {showPopUp[index] &&
+                                <div className='action-drop'>
+                                    <i className="fa-solid fa-x fa-actions" onClick={() => { handleEllipsisClose(index) }}></i>
+                                    <p>More details</p>
+                                    <hr className='hrhr' />
+                                    {(item.status === 'Pending Approval') && <>
+                                        <p
+                                            onClick={()=>{approveProduct(item.id)}}>Approve product</p>
+                                        <hr className='hrhr' />
+                                    </>}
+                                    <p>Update details</p>
+                                    <hr className='hrhr' />
+                                    <p onClick={() => {
+                                        deleteItem(item.id);
+                                        handleEllipsisClose(index)
+                                    }}>Delete products</p>
+                                </div>}
+                        </tr>
+                    ))
+                    }
+                </tbody>
+            </table>
+            {addProd && <AddProduct
+                setAddProd={setAddProd}
+                setProducts={setProducts}
+            />}
+        </div >
+    )
+}
---
+++ src/components/testData/products-test.jsx
@@ -0,0 +1,91 @@
+export const products = [
+    {
+        id: "3RC8",
+        name: "Samsung Galaxy S21",
+        dateAdded: "2022-11-09",
+        description: "6.2 inch, 128GB, 5G, Phantom Gray",
+        remainingPieces: 8,
+        price: 900,
+        category: "Electronics",
+        status: "In Market"
+    },
+    {
+        id: "5CH7",
+        name: "LG OLED TV",
+        dateAdded: "2022-11-09",
+        description: "65 inch, 4K UHD, Smart TV, ThinQ AI",
+        remainingPieces: 3,
+        price: 1500,
+        category: "Electronics",
+        status: "In Market"
+    },
+    {
+        id: "1AC2",
+        name: "Whirlpool Refrigerator",
+        dateAdded: "2022-11-09",
+        description: "French Door, 25 cu. ft., Stainless Steel",
+        remainingPieces: 0,
+        price: 1200,
+        category: "Appliances",
+        status: "Sold Out"
+    },
+    {
+        id: "9PD4",
+        name: "Sony PlayStation 5",
+        dateAdded: "2022-11-09",
+        description: "Disc Version, 825GB SSD",
+        remainingPieces: 5,
+        price: 500,
+        category: "Electronics",
+        status: "In Market"
+    },
+    {
+        id: "7XE6",
+        name: "Dell Inspiron 15",
+        dateAdded: "2022-11-09",
+        description: "15.6 inch, Intel Core i7, 16GB RAM, 512GB SSD ",
+        remainingPieces: 10,
+        price: 1000,
+        category: "Electronics",
+        status: "Pending Approval"
+    },
+    {
+        id: "2LF9",
+        name: "HP OfficeJet Pro Printer",
+        dateAdded: "2022-11-09",
+        description: "All-in-One, Wireless, Fax, Scan, Copy",
+        remainingPieces: 2,
+        price: 250,
+        category: "Electronics",
+        status: "In Market"
+    },
+    {
+        id: "4JG1",
+        name: "Apple MacBook Pro",
+        dateAdded: "2022-11-09",
+        description: "13 inch, M1 Chip, 8-core CPU, 8-core GPU, 512GB SSD",
+        remainingPieces: 4,
+        price: 1500,
+        category: "Electronics",
+        status: "In Market"
+    },
+    {
+        id: "8VZ3",
+        name: "Bose QuietComfort 35 II",
+        dateAdded: "2022-11-09",
+        description: "Wireless, Noise Cancelling, Over-Ear Headphones",
+        remainingPieces: 0,
+        price: 300,
+        category: "Electronics",
+        status: "Sold Out"
+    },
+    {
+        id: "6RD5",
+        name: "Samsung Galaxy Tab S7",
+        dateAdded: "2022-11-09",
+        description: "11 inch, 256GB, Wi-Fi, Mystic Black",
+        remainingPieces: 7,
+        price: 650,
+        category: "Electronics",
+        status: "In Market"
+    },]
