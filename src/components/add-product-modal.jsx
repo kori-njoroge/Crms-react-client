9c3bdbe3b9f8874534b9895c366d02a39e259885
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-30 17:26:15 +0300

    listing catgory from db, and implementation of addint a product and mail service after adding a product

--- src/components/add-product-modal.jsx
+++ src/components/add-product-modal.jsx
@@ -1,19 +1,42 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 
 import logo from '../assets/logo.png'
+import { apiproducts } from './api-links';
 
-export default function AddProduct({ setAddProd, setProducts }) {
-    const[message, setMessage] = useState('')
+export default function AddProduct({ setAddProd }) {
+    const [loading, setLoading] = useState(false)
+    let token = window.localStorage.getItem('token')
+    let user = JSON.parse(window.localStorage.getItem('user'))
+    const [categories, setCategories] = useState([])
+    const [message, setMessage] = useState('')
     const [prodDetails, setProdDetails] = useState(
         {
             name: '',
             description: '',
             category: '',
             itmesNo: '',
-            price: ''
+            price: '',
+            role: user?.role,
+            addedBy: user?.id
         }
     )
+    useEffect(() => {
+        async function getData() {
+            await axios.get(`${apiproducts}/category/all-categories`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'Application/json'
+                }
+            }).then(reply => {
+                setCategories(reply?.data)
+            }).catch(err => {
+                setMessage(err.response.data.message)
+            })
+        }
+        getData()
+    }, [])
 
 
     function handleOnchange(event) {
@@ -27,19 +50,54 @@ export default function AddProduct({ setAddProd, setProducts }) {
     }
     function handleSubmit(event) {
         event.preventDefault();
-        let arr = {
-            id: 'AD54',
-            status: 'Pending Approval',
-            remainingPieces: prodDetails.itmesNo
-        }
-        let modified = Object.assign(prodDetails, arr)
-        console.log(modified)
-        setProducts(prevState => [...prevState, modified]);
-        setMessage('Product added successfully')
+        setLoading(true)
+        axios.post(`${apiproducts}/add`, {
+            "role": user?.role,
+            "productName": prodDetails.name,
+            "description": prodDetails.description,
+            "addedBy": user?.id,
+            "price": prodDetails.price,
+            "quantity": prodDetails.itmesNo,
+            "categoryId": prodDetails.category,
+            "token": token
+
+        }, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'Application/json'
+            }
+        }).then(reply => {
+            setMessage(reply.data.message)
+            setLoading(false)
+            setProdDetails({
+                name: '',
+                description: '',
+                category: '',
+                itmesNo: '',
+                price: ''
+            })
+        }).catch(err => {
+            setLoading(false)
+            setMessage(err.response.data.message)
+        })
 
     }
 
-    // console.log(prodDetails)
+
+    useEffect(() => {
+        function handleKeyDown(event) {
+            if (event.keyCode === 27) {
+                setAddProd(false);
+            }
+        }
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [setAddProd]);
+    function handleOnfocus(){
+        setMessage('')
+    }
     return (
         <div className='modal'>
             <div className="info-container">
@@ -56,6 +114,7 @@ export default function AddProduct({ setAddProd, setProducts }) {
                                 name="name"
                                 value={prodDetails.name}
                                 onChange={handleOnchange}
+                                onFocus={handleOnfocus}
                                 required />
                             <label htmlFor="number">Number of items:</label>
                             <input
@@ -64,6 +123,7 @@ export default function AddProduct({ setAddProd, setProducts }) {
                                 id="number"
                                 value={prodDetails.itmesNo}
                                 onChange={handleOnchange}
+                                onFocus={handleOnfocus}
                                 required />
                         </div>
                         <div className="">
@@ -73,11 +133,13 @@ export default function AddProduct({ setAddProd, setProducts }) {
                                 id="category"
                                 value={prodDetails.category}
                                 onChange={handleOnchange}
-                                required>
-                                <option value="electronics">Electronics</option>
-                                <option value="men's clothes">Men's clothes</option>
-                                <option value="women's clothes">Women's clothes</option>
-                                <option value="shoes">Shoes</option>
+                                onFocus={handleOnfocus}
+                                required
+                            >
+                                {categories &&
+                                    categories.map((category, index) => (
+                                        <option style={{ textTransform: 'capitalize !important' }} key={index} value={category.id}>{category.name}</option>
+                                    ))}
                             </select>
                             <label htmlFor="price">Price per item:</label>
                             <input
@@ -86,10 +148,11 @@ export default function AddProduct({ setAddProd, setProducts }) {
                                 id="price"
                                 value={prodDetails.price}
                                 onChange={handleOnchange}
+                                onFocus={handleOnfocus}
                                 required />
                         </div>
                     </div>
-                    {message && <p className={message === "Product added successfully"?  'valid validvalid' : "invalid validvalid"}>{message}</p>}
+                    {message && <p className={message?.toLowerCase().includes('successfully') ? 'valid validvalid' : "invalid validvalid"}>{message}</p>}
                     <div className='tt-area'>
                         <label htmlFor="description">Description:</label>
                         <textarea
@@ -97,10 +160,15 @@ export default function AddProduct({ setAddProd, setProducts }) {
                             id="description"
                             value={prodDetails.description}
                             onChange={handleOnchange}
+                            onFocus={handleOnfocus}
                         ></textarea> <br />
                     </div>
-                    
-                    {message ==="Product added successfully"? <button type='button' onClick={()=>{setAddProd(false)}}>Exit</button> :<button>Add Product</button> }
+                    {loading ? <div className='donut-wrapper donut-users'>
+                        <div className='donut-product multi'></div>
+                    </div>
+                        : ""}
+
+                    {message === "Product added successfully" ? <button type='button' onClick={() => { setAddProd(false) }}>Exit</button> : <button>Add Product</button>}
                 </form>
             </div>
         </div>
