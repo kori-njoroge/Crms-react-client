fba600f1719518beb7f17579908c1eb9a2b00dd0
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-30 17:27:28 +0300

    staff dashboard implementation

---
+++ src/pages/staff/staff-dashboard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Navbar from '../../components/navbar'
+import Sidebar from '../../components/sidebar'
+
+import '../../styles/admin-dashboard.css'
+
+export default function StaffDashboard() {
+    const navigate = useNavigate()
+    return (
+        <div className='admin-dashboard'>
+            <div className="navnav">
+                <Navbar />
+            </div>
+            <button
+                onClick={() => { navigate(-1) }}
+                className="back-btn"
+            >
+                <i className="fa-solid fa-chevron-left"></i>
+                &nbsp; Back
+            </button>
+            <br />
+            <div className="dashboard-body">
+                <div className="sidebar">
+                    <Sidebar />
+                </div>
+                <div className="outlet">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    )
+}
