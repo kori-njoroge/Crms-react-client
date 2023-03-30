4bb9a000882eecece3e9e04321180c890884222b
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-31 01:06:24 +0300

    datalist for users when searching for user

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import AddUserModal from './addusermodal';
-import { users } from './testData/logintest'
+import { apiusers } from './api-links';
 
-export default function Checkout({setSelectedProds}) {
+export default function Checkout({ setSelectedProds }) {
+    const [customers, setCustomers] = useState()
     const [addUser, setAdduser] = useState(false);
     const [phone, setPhone] = useState('');
     const [customer, setCustomer] = useState({});
 
+
+    useEffect(() => {
+        let token = window.localStorage.getItem('token')
+        async function fetchData() {
+            axios.get(`${apiusers}/`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+            ).then(response => {
+                setCustomers(response.data)
+                console.log(response.data)
+            }).catch(err => {
+                console.log(err.response)
+            })
+        }
+        fetchData();
+    }, [])
+
     function searchCustomer() {
-        let customer = users.filter(customer => customer.phonenumber === phone);
+        let customer = customers.filter(customer => customer.phone === phone);
         setCustomer(customer[0])
     }
 
@@ -20,17 +42,24 @@ export default function Checkout({setSelectedProds}) {
                     type="tel"
                     name="search"
                     placeholder='phone number'
+                    list='brow2'
                     onChange={(event) => {
                         setPhone(event.target.value)
                     }}
                 />
                 <button onClick={searchCustomer}>Search</button>
+                <datalist id="brow2" className="data--list">
+                    {customers ?
+                        customers.map((customer, index) => (
+                            <option key={index} value={`${customer.phone}`} >{`${customer.phone}  ${customer.full_name}`}</option>
+                        )) : 'Nothing'}
+                </datalist>
             </div>
             <div className="results-search">
                 {customer ?
                     <>
                         <div>
-                            <p>Name:&nbsp;{customer.fullname}</p>
+                            <p>Name:&nbsp;{customer.full_name}</p>
                             <p>Email: &nbsp;{customer.email}</p>
                         </div>
                         <button
